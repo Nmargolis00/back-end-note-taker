0dd1e80632b288bfd1a294913ad8b96225283a1e
Nmargolis00 <119348315+Nmargolis00@users.noreply.github.com>
2023-03-07 12:58:14 -0700

    got rid of develop folder

--- Main/server.js
+++ Main/server.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
-const fs = require('fs');
-const newNotes = require('./db/db.json'); //Added 3/7/2023
-const uniqid = require('uniqid'); //Creates a new id for db file
+
 
 // Initialize the app and create a port
 const app = express();
@@ -21,24 +19,6 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html
 
 // GET Route for Notes Page
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname,'/public/notes.html')));
-
-// *GET API route for loading saved notes on left side of screen
-app.get('/notes', (req, res) => res.json(newNotes));
-
-// * Post API Route for when a new note is saved. Needs to include file path to db file
-
-// post Route for api request (added 3/7/2023)
-app.post('/notes', (req, res) => {
-    res.json(`${req.method} request received`);
-    console.info(req.rawHeaders);
-    console.info(`${req.method} request received`);
-})
-
-//GET /api/notes should read the db.json file and return all saved notes as JSON.
-
-
-//POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
-
 
 
 // Start the server on the port
